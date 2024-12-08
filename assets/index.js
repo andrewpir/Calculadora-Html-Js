bdec33f28d7d17d50492df5177c7b05586f5db18
Andrew Pires <andrewpir@gmail.com>
2024-12-07 21:15:11 -0300

    First Commit

---
+++ assets/index.js
@@ -0,0 +1,18 @@
+var a1 = getElementById("c1");
+var a2 = getElementById("c2");
+var a3 = getElementById("c3");
+var a4 = getElementById("c4");
+var a5 = getElementById("c5");
+var a6 = getElementById("c6");
+var a7 = getElementById("c7");
+var a8 = getElementById("c8");
+var a9 = getElementById("c9");
+var a0 = getElementById("c0");
+
+var igual = getElementById("igual")
+
+var disp = getElementById("disp")
+igual.innerText = "teste"
+if (disp) {
+    
+}
